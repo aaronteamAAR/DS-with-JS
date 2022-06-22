25f043665fbdbfb4297cd5a3de210f33e78454c2
Aaron Chris <rooneychris83@gmail.com>
2022-06-22 10:32:46 +0100

    Worked on array DS

---
+++ ds.js
@@ -0,0 +1,22 @@
+const names = ['Max', 'James', 'Smart', 'john']
+console.log(names[3])
+console.log(names.length)
+for (const el of names){
+    console.log(el)
+}
+
+names.push('Aaron')
+
+console.log(names)
+
+names.find(el => el === 'Aaron')
+
+console.clear()
+
+
+names.push('Aaron')
+
+console.log(names)
+
+const aaron = names.findIndex(el => el === 'Aaron')
+console.warn(aaron)
